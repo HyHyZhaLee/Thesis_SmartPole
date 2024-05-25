126454a8feca98d5588be9d631c265b69f5b1b4b
HyHyZhaLee <huy.ly2053038@hcmut.edu.vn>
2024-05-25 11:01:54 +0700

    Commit app

---
+++ FlutterApp/lib/main - Copy.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'dart:convert'; // To use jsonEncode and jsonDecode
+import 'AppFunction/mqtt_helper.dart';
+import 'Widgets/custom_slider_widget.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+// Your MQTT details
+String mqttServer = "mqttserver.tk";
+String mqttClientId = "Flutter_app";
+String mqttUserName = "innovation";
+String mqttPassword = "Innovation_RgPQAZoA5N";
+String mqttTopic = "/innovation/airmonitoring/SmartPole";
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter MQTT Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MyHomePage(title: 'MQTT Light Control'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late MQTTHelper _mqttHelper;
+  double _brightness = 0;
+  double _brightness2 = 0; // Slider value for second device
+  bool _isLightOn = false;
+  String _statusMessage = 'Disconnected';
+
+  @override
+  void initState() {
+    super.initState();
+    _mqttHelper = MQTTHelper(mqttServer, mqttClientId, mqttUserName, mqttPassword);
+    _initializeMQTT();
+  }
+
+  void _initializeMQTT() async {
+    bool isConnected = await _mqttHelper.initializeMQTTClient();
+    if (isConnected) {
+      _mqttHelper.subscribe(mqttTopic, _handleReceivedMessage);
+      setState(() {
+        _statusMessage = 'Connected';
+      });
+    } else {
+      setState(() {
+        _statusMessage = 'Connection Failed';
+      });
+    }
+  }
+
+  void _handleReceivedMessage(dynamic message) {
+    print("Handling message: $message");
+    try {
+      // Decode the message into a map
+      Map<String, dynamic> msg = jsonDecode(message);
+      // Check the contents of the message before proceeding
+      if (msg['station_id'] == 'SmartPole_0002' &&
+          msg['station_name'] == 'Smart Pole 0002' &&
+          msg['action'] == 'control light') {
+        // Safely try to parse the brightness value
+        double brightnessValue = double.tryParse(msg['data'].toString()) ?? _brightness;
+        setState(() {
+          _brightness = brightnessValue;
+          _isLightOn = _brightness > 0;  // Update the light state based on brightness
+          print("Brightness setting: $brightnessValue");
+        });
+      } else {
+        print("Message does not match required criteria.");
+      }
+    } catch (e) {
+      print('Error processing received message: $e');
+    }
+  }
+
+  void _publishControlMessage(double brightness, String deviceId) {
+    var message = jsonEncode({
+      "station_id": "SmartPole_0002",
+      "station_name": "Smart Pole 0002",
+      "action": "control light",
+      "device_id": deviceId,
+      "data": brightness.round().toString()
+    });
+    _mqttHelper.publish(mqttTopic, message);
+  }
+
+  void _updateBrightness(double brightness, String deviceId) {
+    setState(() {
+      if (deviceId == "NEMA_0002") {
+        _brightness = brightness;
+      } else if (deviceId == "NEMA_0003") {
+        _brightness2 = brightness;
+      }
+      _isLightOn = brightness > 0;
+    });
+    _publishControlMessage(brightness, deviceId);
+  }
+
+  @override
+  void dispose() {
+    _mqttHelper.disconnect();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Color(0xFF1488DB), // Set the AppBar background color
+        titleTextStyle: const TextStyle(
+        color: Colors.white, // Set the title text color to white
+        fontSize: 24, // Increase the font size for better visibility
+        fontWeight: FontWeight.bold, // Make the title bold
+        ),
+        elevation: 4, // Optional: Adds shadow below the AppBar
+        centerTitle: true, // Optional: Centers the title
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: CustomSliderWidget(
+                      initialSliderValue: _brightness,
+                      onValueChanged: (value) {
+                        _updateBrightness(value, "NEMA_0002");  // Updates for device NEMA_0002
+                      },
+                      deviceName: "NEMA 0002",
+                    ),
+                  ),
+                  Expanded(
+                    child: CustomSliderWidget(
+                      initialSliderValue: _brightness2,
+                      onValueChanged: (value) {
+                        _updateBrightness(value, "NEMA_0003");  // Updates for device NEMA_0003
+                      },
+                      deviceName: "NEMA 0003",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Additional controls can be placed here if needed
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ FlutterApp/lib/pages/home_page.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_app/util/smart_device_box.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // padding constants
+  final double horizontalPadding = 40;
+  final double verticalPadding = 25;
+
+  // list of smart devices
+  List mySmartDevices = [
+    // [ smartDeviceName, iconPath , powerStatus ]
+    ["NEMA 0001", "lib/icons/light-bulb.png", false],
+    ["NEMA 0002", "lib/icons/light-bulb.png", false],
+  ];
+
+  // power button switched
+  void powerSwitchChanged(bool value, int index) {
+    setState(() {
+      mySmartDevices[index][2] = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // app bar
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: horizontalPadding,
+                vertical: verticalPadding,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // menu icon
+                  Image.asset(
+                    'lib/icons/menu.png',
+                    height: 45,
+                    color: Colors.grey[800],
+                  ),
+
+                  // account icon
+                  Icon(
+                    Icons.person,
+                    size: 45,
+                    color: Colors.grey[800],
+                  )
+                ],
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            // welcome home
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Welcome",
+                    style: TextStyle(fontSize: 20, color: Colors.grey.shade800),
+                  ),
+                  Text(
+                    'SMART POLE',
+                    style: GoogleFonts.bebasNeue(fontSize: 72),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 25),
+
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0),
+              child: Divider(
+                thickness: 1,
+                color: Color.fromARGB(255, 204, 204, 204),
+              ),
+            ),
+
+            SizedBox(height: 25),
+
+            // smart devices grid
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+              child: Text(
+                "Smart Devices",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  color: Colors.grey.shade800,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+
+            // grid
+            SizedBox(
+              height: 600, // Đặt chiều cao cụ thể cho GridView
+              child: GridView.builder(
+                itemCount: mySmartDevices.length,
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 1 / 1.3,
+                ),
+                itemBuilder: (context, index) {
+                  return SmartDeviceBox(
+                    smartDeviceName: mySmartDevices[index][0],
+                    iconPath: mySmartDevices[index][1],
+                    powerOn: mySmartDevices[index][2],
+                    onChanged: (value) => powerSwitchChanged(value, index),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
