542ebb01a0dbfb316ca27932afa80bdac4fd2539
HyHyZhaLee <huy.ly2053038@hcmut.edu.vn>
2024-06-19 11:44:17 +0700

    Git update function

--- FlutterApp/lib/AppFunction/global_variables.dart
+++ FlutterApp/lib/AppFunction/global_variables.dart
@@ -6,5 +6,6 @@ const String MQTT_SERVER = "mqtt.ohstem.vn";
 const String MQTT_USERNAME = "BK_SmartPole";
 const String MQTT_PASSWORD = " ";
 const String MQTT_TOPIC = "BK_SmartPole/feeds/V20";
+const SENSOR_DATA_URL = "https://ezdata2.m5stack.com/api/v2/4827E2E30938/dataMacByKey/raw";
 
 late MQTTHelper global_mqttHelper;
