b3dc706da23922f39332245109f0927075160526
HyHyZhaLee <huy.ly2053038@hcmut.edu.vn>
2024-11-01 15:08:58 +0700

    success changing the icon

--- FlutterApp/lib/widgets/navigation_drawer.dart
+++ FlutterApp/lib/widgets/navigation_drawer.dart
@@ -3,10 +3,10 @@ import 'package:flutter_app/AppFunction/global_variables.dart';
 
 const double space_between_icon = 30.0;
 const double space = 10.0;
+
 class CustomNavigationDrawer extends StatefulWidget {
   final int selectedIndex;
   final Function(int) onDestinationSelected;
-
 
   const CustomNavigationDrawer({
     super.key,
@@ -45,7 +45,7 @@ class _CustomNavigationDrawerState extends State<CustomNavigationDrawer> with Si
     return LayoutBuilder(
       builder: (context, constraints) {
         return Container(
-          margin: USE_BORDER_RADIUS? const EdgeInsets.all(10): const EdgeInsets.all(0),
+          margin: USE_BORDER_RADIUS ? const EdgeInsets.all(10) : const EdgeInsets.all(0),
           child: MouseRegion(
             onEnter: (_) => _handleMouseEnter(true),
             onExit: (_) => _handleMouseEnter(false),
@@ -63,7 +63,10 @@ class _CustomNavigationDrawerState extends State<CustomNavigationDrawer> with Si
                           flex: 190,
                           child: Container(
                             margin: const EdgeInsets.only(top: 67),
-                            child: _buildDrawerItem(0, Icons.home, 'Home'),
+                            child: _buildDrawerItem(0, Icons.home, 'Home',
+                              clickedIconPath: 'lib/Assets/icons/page_icons/home_clicked.png',
+                              unclickedIconPath: 'lib/Assets/icons/page_icons/home_unclicked.png',
+                            ),
                           ),
                         ),
                         Expanded(
@@ -72,31 +75,52 @@ class _CustomNavigationDrawerState extends State<CustomNavigationDrawer> with Si
                             children: [
                               Container(
                                 margin: const EdgeInsets.only(bottom: space_between_icon, top: space_between_icon),
-                                child: _buildDrawerItem(1, Icons.apps, 'Main Dashboard'),
+                                child: _buildDrawerItem(1, Icons.apps, 'Main Dashboard',
+                                  clickedIconPath: 'lib/Assets/icons/page_icons/MainDashboard_clicked.png',
+                                  unclickedIconPath: 'lib/Assets/icons/page_icons/MainDashboard_unclicked.png',
+                                ),
                               ),
                               Container(
-                                margin: const EdgeInsets.only(bottom: space_between_icon ),
-                                child: _buildDrawerItem(2, Icons.lightbulb_outline, 'Light Control'),
+                                margin: const EdgeInsets.only(bottom: space_between_icon),
+                                child: _buildDrawerItem(2, Icons.lightbulb_outline, 'Light Control',
+                                  clickedIconPath: 'lib/Assets/icons/page_icons/LightControl_clicked.png',
+                                  unclickedIconPath: 'lib/Assets/icons/page_icons/LightControl_unclicked.png',
+                                ),
                               ),
                               Container(
-                                margin: const EdgeInsets.only(bottom: space_between_icon ),
-                                child: _buildDrawerItem(3, Icons.schedule, 'Lighting Schedule'),
+                                margin: const EdgeInsets.only(bottom: space_between_icon),
+                                child: _buildDrawerItem(3, Icons.schedule, 'Lighting Schedule',
+                                  clickedIconPath: 'lib/Assets/icons/page_icons/LightingSchedule_clicked.png',
+                                  unclickedIconPath: 'lib/Assets/icons/page_icons/LightingSchedule_unclicked.png',
+                                ),
                               ),
                               Container(
-                                margin: const EdgeInsets.only(bottom: space_between_icon ),
-                                child: _buildDrawerItem(4, Icons.videocam, 'Security Cameras'),
+                                margin: const EdgeInsets.only(bottom: space_between_icon),
+                                child: _buildDrawerItem(4, Icons.videocam, 'Security Cameras',
+                                  clickedIconPath: 'lib/Assets/icons/page_icons/sercurity_clicked.png',
+                                  unclickedIconPath: 'lib/Assets/icons/page_icons/sercurity_unclicked.png',
+                                ),
                               ),
                               Container(
-                                margin: const EdgeInsets.only(bottom: space_between_icon ),
-                                child: _buildDrawerItem(5, Icons.tv, 'Advertisement Schedule'),
+                                margin: const EdgeInsets.only(bottom: space_between_icon),
+                                child: _buildDrawerItem(5, Icons.tv, 'Advertisement Schedule',
+                                  clickedIconPath: 'lib/Assets/icons/page_icons/advertisement_clicked.png',
+                                  unclickedIconPath: 'lib/Assets/icons/page_icons/advertisement_unclicked.png',
+                                ),
                               ),
                               Container(
-                                margin: const EdgeInsets.only(bottom: space_between_icon ),
-                                child: _buildDrawerItem(6, Icons.thermostat, 'Environmental Sensors'),
+                                margin: const EdgeInsets.only(bottom: space_between_icon),
+                                child: _buildDrawerItem(6, Icons.thermostat, 'Environmental Sensors',
+                                  clickedIconPath: 'lib/Assets/icons/page_icons/environment_clicked.png',
+                                  unclickedIconPath: 'lib/Assets/icons/page_icons/environment_unclicked.png',
+                                ),
                               ),
                               Container(
-                                margin: const EdgeInsets.only(bottom: space_between_icon ),
-                                child: _buildDrawerItem(7, Icons.bar_chart, 'Historical Data'),
+                                margin: const EdgeInsets.only(bottom: space_between_icon),
+                                child: _buildDrawerItem(7, Icons.bar_chart, 'Historical Data',
+                                  clickedIconPath: 'lib/Assets/icons/page_icons/historical_clicked.png',
+                                  unclickedIconPath: 'lib/Assets/icons/page_icons/historical_unclicked.png',
+                                ),
                               ),
                             ],
                           ),
@@ -104,7 +128,11 @@ class _CustomNavigationDrawerState extends State<CustomNavigationDrawer> with Si
                         Expanded(
                           flex: 102,
                           child: Container(
-                            child: _buildDrawerItem(8, Icons.exit_to_app, 'Exit'),
+                            margin: const EdgeInsets.only(bottom: space_between_icon),
+                            child: _buildDrawerItem(8, Icons.info, 'More Information',
+                              clickedIconPath: 'lib/Assets/icons/page_icons/moreInformation_clicked.png',
+                              unclickedIconPath: 'lib/Assets/icons/page_icons/moreInformation_unclicked.png',
+                            ),
                           ),
                         ),
                       ],
@@ -119,15 +147,20 @@ class _CustomNavigationDrawerState extends State<CustomNavigationDrawer> with Si
     );
   }
 
-  Widget _buildDrawerItem(int index, IconData icon, String title) {
+  Widget _buildDrawerItem(int index, IconData? icon, String title, {String? clickedIconPath, String? unclickedIconPath}) {
     bool isSelected = widget.selectedIndex == index;
     return ListTile(
       selected: isSelected,
       selectedTileColor: const Color(0xFFF9F9F9),
       leading: Padding(
-        //padding left 16
-        padding: const EdgeInsets.only(left: 8),
-        child: Icon(icon, color: Colors.white, size: 35),
+        padding: const EdgeInsets.only(left: 8, right: 8),
+        child: clickedIconPath != null && unclickedIconPath != null
+            ? Image.asset(
+          isSelected ? clickedIconPath : unclickedIconPath,
+          width: 35,
+          height: 35,
+        )
+            : Icon(icon, color: Colors.white, size: 23),
       ),
       title: _isExpanded
           ? Text(
@@ -135,12 +168,9 @@ class _CustomNavigationDrawerState extends State<CustomNavigationDrawer> with Si
         style: TextStyle(fontSize: 20, color: isSelected ? Colors.black : Colors.white),
         overflow: TextOverflow.ellipsis,
         maxLines: 1,
-        //text size = 20
-
       )
           : null,
       onTap: () => widget.onDestinationSelected(index),
     );
   }
-
 }
